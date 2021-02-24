332169a1bd823b31e47a1fff9bf8f64f1af04eb1
Giorgio <shadigiorgioislam@gmail.com>
2021-02-24 16:39:43 +0100

    init

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+  $testo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+  // $newName = str_replace($badWord, "***", $testo);
+  $array = explode(" ", $testo);
+  $length = sizeof($array);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <h1><?php echo $testo ?></h1>
+  <h2>Il testo contiene <?php echo $length ?> parole</h2>
+</body>
+</html>
