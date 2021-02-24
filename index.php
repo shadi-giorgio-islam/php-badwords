4947e5d96b5dccbc8654586b8c706736d09f9961
Giorgio <shadigiorgioislam@gmail.com>
2021-02-24 17:04:25 +0100

    Add length and text

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-  $testo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-  // $newName = str_replace($badWord, "***", $testo);
-  $array = explode(" ", $testo);
+  $text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+  $newtext = str_replace(" sit", "***", $text);
+
+  $array = explode(" ", $text);
   $length = sizeof($array);
 ?>
 <!DOCTYPE html>
@@ -10,10 +11,12 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>Primo esercizio php</title>
 </head>
 <body>
-  <h1><?php echo $testo ?></h1>
+  <h1><?php echo $newtext ?></h1>
   <h2>Il testo contiene <?php echo $length ?> parole</h2>
+  <h3>la badword è <?php echo $GET['word']?></h3>
+  <h4><?php echo var_dump($GET); ?></h4>
 </body>
 </html>
